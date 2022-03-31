c57352417d6c4843bcc4374d3c8c0c44a048550f
Carlos Lecaro <calgcarloslecaro@gmail.com>
2022-03-31 16:57:12 -0500

    [ADD] Numeric System

---
+++ numeric_system/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+	int _integer = 15; //Decimal
+	int _octal = 017;
+	int _headecimal = 0x0F;
+	int _binary = 0b00001111;
+
+	std::cout << "Integer: " << _integer << std::endl;
+	std::cout << "Octal: " << _octal  << std::endl;
+	std::cout << "Hexadecimal: " << _headecimal  << std::endl;
+	std::cout << "Binary: " << _binary  << std::endl;
+
+
+
+
+
+	return 0;
+}
